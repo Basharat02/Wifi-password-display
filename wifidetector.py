badd2695662bac8185922097ddac2626033c43d1
Basharat02 <52979182+Basharat02@users.noreply.github.com>
2020-06-08 01:19:53 +0500

    Update wifidetector.py

--- wifidetector.py
+++ wifidetector.py
@@ -1,9 +1,16 @@
 
-
+#subprocess is a python module which as the name suggests provides the interface for working with processes.Import this module
+#in order to run this script.
 import subprocess
+
+# subprocess.check_output() will run the command (netsh wlan show profiles) in the command prompt. 
+#It will save the results in our variable data. As you can see it is similar to os.command().
+# We use check_output() to capture the output for later processing.
 
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+
+#Text formatting allows us to seperate the desired text in a formatted way.
 for i in profiles:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
@@ -13,9 +20,12 @@ for i in profiles:
         print ("{:<30}|  {:<}".format(i, ""))
 
 
+#subprocess.check_output will call ipconfig.
+#It will show the same result as if you were typing ipconfig in the command prompt in case of windows.
+
 data = subprocess.check_output(['ipconfig']).decode('utf-8').split('/n')
 
-
+#Some other text formatting to display the python result in a formatted way.
 for b in data :
     stringer = ''
     if "IPv4 Address" in b:
@@ -23,6 +33,9 @@ for b in data :
         stringer = stringer + b
         
 print(i)
+
+#We have used python file module here to create a new text file and write all the results obtained from above mentioned 
+#commands and close the file.
 
 file = open('text.txt','w')
 file.write(b)
